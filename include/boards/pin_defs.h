cd5267014c13c20842bcac37394c0978ea4466e0
Michael Pritchard <michael.pritchard@csiro.au>
2022-03-01 16:49:45 +1100

    pin defs change

--- include/boards/pin_defs.h
+++ include/boards/pin_defs.h
@@ -9,18 +9,24 @@
 #define LED_BUILTIN PC13
 #endif
 // motor pins
-#define L_MOTOR_PIN_0 PD3
-#define L_MOTOR_PIN_1 PD4
-#define R_MOTOR_PIN_0 PD3
-#define R_MOTOR_PIN_1 PD4
+#define L_MOTOR_PIN_0 PB6 // tx D1
+#define L_MOTOR_PIN_1 PD4 // (e stop)
+#define R_MOTOR_PIN_0 PB6 // tx D1
+#define R_MOTOR_PIN_1 PD4 // (e stop)
 // encoder pins
-#define L_ENC_PIN_0 PD5
-#define L_ENC_PIN_1 PD6
-#define R_ENC_PIN_0 PD7
-#define R_ENC_PIN_1 PD8
+#define L_ENC_PIN_0 PD7
+#define L_ENC_PIN_1 PD8
+#define R_ENC_PIN_0 PD9
+#define R_ENC_PIN_1 PD10
+#define LB_ENC_PIN_0 PD11
+#define LB_ENC_PIN_1 PD12
+#define RB_ENC_PIN_0 PD13
+#define RB_ENC_PIN_1 PD14
 // interrupt pins
 #define L_ENC_PIN_INT L_ENC_PIN_0
 #define R_ENC_PIN_INT R_ENC_PIN_0
+#define LB_ENC_PIN_INT LB_ENC_PIN_0
+#define RB_ENC_PIN_INT RB_ENC_PIN_0
 // serial
 #define TMC_SERIAL_CONFIG Serial
 // #define TMC_SERIAL_CONTROL Serial3
